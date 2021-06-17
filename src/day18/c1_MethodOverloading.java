31a981d74b6fbc80c8bc3496668d8d42d046ce12
Rustem-Erdem <rustemerdem129@gmail.com>
2021-06-16 19:07:50 -0500

    overloadings

--- src/day18/c1_MethodOverloading.java
+++ src/day18/c1_MethodOverloading.java
@@ -1,2 +1,25 @@
-package day18;public class c1_MethodOverloading {
+package day18;
+
+public class c1_MethodOverloading {
+    public static void main(String[] args) {
+        int n1=25;
+        double n2=1.56;
+        byte n3=2;
+
+        method(n2);
+        method(n3);
+        method("rustem");
+    }
+    public static void method(int a){
+        System.out.println(a+ ": integer");
+    }
+    public static void method(double a){
+        System.out.println(a+ " : double");
+    }
+    public static void method(byte a){
+        System.out.println(a+ ": byte");
+    }
+    public static void method(String a){
+        System.out.println(a+ ": string");
+    }
 }
---
+++ src/day18/c3_Overloading3.java
@@ -0,0 +1,4 @@
+package day18;
+
+public class c3_Overloading3 {
+}
